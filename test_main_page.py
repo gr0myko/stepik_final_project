42daaa1d399f5cd0a826176decc84bf04dcace3b
gr0m.ko <a.gromyko.a@gmail.com>
2020-07-20 11:25:06 +0300

    check login link

--- test_main_page.py
+++ test_main_page.py
@@ -5,8 +5,11 @@ def test_guest_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    #login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
-def test_quest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
